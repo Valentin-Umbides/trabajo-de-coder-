0b775946dfa65a59ebbfe7b176830f3f0ffb7238
Valentin Umbides Alvarez <valentinumbides8@gmail.com>
2023-03-14 23:47:12 -0300

    trabajo react

--- src/App.js
+++ src/App.js
@@ -11,20 +11,11 @@ function App() {
   
     <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          
+          
           <NavBar />
           <ItemListContainer/>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     
